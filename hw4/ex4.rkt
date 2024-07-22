59b5dc559de6344415710ff457b2cc6c3af71b38
Idan Saltzman <saltzman@post.bgu.ac.il>
2024-07-22 18:13:20 +0300

    hw4 done I DID NOT BOTHER TESTING B^)

--- hw4/ex4.rkt
+++ hw4/ex4.rkt
@@ -26,7 +26,7 @@
 ; Signature: pipe(lst-fun)
 ; Type: [[T1 -> T2],[T2 -> T3]...[Tn-1 -> Tn]]  -> [T1->Tn]
 ; Purpose: Returns the composition of a given list of unary functions. For (pipe (list f1 f2 ... fn)), returns the composition fn(....(f1(x)))
-; test: ((pipe (list sqrt - - number?)) 16)) ==> true
+; test: ((pipe (list sqrt - - number?)) 16) ==> true
 ;       ((pipe (list sqrt - - number? not)) 16) ==> false
 ;       ((pipe (list sqrt add1 - )) 100) ==> -11
 (define pipe
@@ -36,6 +36,7 @@
         (compose (car fs) (pipe (cdr fs))))))
 
 ; Signature: pipe$(lst-fun,cont)
+; Type: [
 ;         [T1 * [T2->T3] ] -> T3,
 ;         [T3 * [T4 -> T5] ] -> T5,
 ;         ...,
@@ -46,55 +47,77 @@
 ;              [[T1 * [T2n+1 -> T2n+2]] -> T2n+2]
 ;      -> [T1 * [T2n+1 -> T2n+2]] -> T2n+2
 ; Purpose: Returns the composition of a given list of unry CPS functions. 
-(define pipe$  '...)
-
+(define pipe$ 
+    (lambda (fs cont)
+        (if (empty? (cdr fs))
+            (cont (car fs))
+            (pipe$ (cdr fs) (lambda (x) (cont ((car fs) x)))))))
 
 ;;; Q2a
 ; Signature: reduce1-lzl(reducer, init, lzl) 
 ; Type: [T2*T1 -> T2] * T2 * LzL<T1> -> T2
 ; Purpose: Returns the reduced value of the given lazy list
-(define reduce1-lzl 
-  (lambda (reducer init lzl)
-   #f ;@TODO
-  )
-)  
+(define reduce1-lzl
+    (lambda (reducer init lzl)
+        (if (empty-lzl? lzl)
+            init
+            (reducer (head lzl) (reduce1-lzl reducer init (tail lzl)))
+        )
+    )
+)
 
 ;;; Q2b
 ; Signature: reduce2-lzl(reducer, init, lzl, n) 
 ; Type: [T2*T1 -> T2] * T2 * LzL<T1> * Number -> T2
 ; Purpose: Returns the reduced value of the first n items in the given lazy list
-(define reduce2-lzl 
-  (lambda (reducer init lzl n)
-    #f ;@TODO
-  )
-)  
+(define reduce2-lzl
+    (lambda (reducer init lzl n)
+        (if (or (empty-lzl? lzl) (= n 0))
+            init
+            (reducer (head lzl) (reduce2-lzl reducer init (tail lzl) (- n 1)))
+        )
+    )
+)
 
 ;;; Q2c
 ; Signature: reduce3-lzl(reducer, init, lzl) 
 ; Type: [T2 * T1 -> T2] * T2 * LzL<T1> -> Lzl<T2>
 ; Purpose: Returns the reduced values of the given lazy list items as a lazy list
-(define reduce3-lzl 
-  (lambda (reducer init lzl)
-    #f ;@TODO
-  )
-)  
+(define reduce3-lzl
+    (lambda (reducer init lzl)
+        (if (empty-lzl? lzl)
+            empty-lzl
+            (cons-lzl (reducer init (head lzl)) (lambda () (reduce3-lzl reducer init (tail lzl))))
+        )
+    )
+)
  
 ;;; Q2e
 ; Signature: integers-steps-from(from,step) 
 ; Type: Number * Number -> Lzl<Number>
 ; Purpose: Returns a list of integers from 'from' with 'steps' jumps
 (define integers-steps-from
-  (lambda (from step)
-    #f ; @TODO
-  )
+    (lambda (from step)
+        (cons-lzl from (lambda () (integers-steps-from (+ from step) step)))
+    )
 )
 
 ;;; Q2f
 ; Signature: generate-pi-approximations() 
 ; Type: Empty -> Lzl<Number>
 ; Purpose: Returns the approximations of pi as a lazy list
+; use the following series: 1/a×(a+2) + 1/(a+4)×(a+6) + 1/(a+8)∗(a+10) +… = π/8
 (define generate-pi-approximations
-  (lambda ()
-    #f ; @TODO
-   )
- )
+    (lambda ()
+        (letrec
+            (
+                (helper
+                    (lambda (a)
+                        (cons-lzl (/ 1.0 (* a (+ a 2))) (lambda () (helper (+ a 4))))
+                    )
+                )
+            (helper 1)
+            )
+        )
+    )
+)
